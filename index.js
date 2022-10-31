3fb0e8f98d70c47e088e9675895824f92e40e867
SeungWoo <wjknn3123@gmail.com>
2022-10-31 16:57:38 +0900

    [UPDATE] Footer Edit

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ window.addEventListener('load', function (){
    let homeBtn = document.getElementById('home');
    let introBtn = document.getElementById('introduce');
    homeBtn.addEventListener('click', function (){
-      window.location = './index.html'
-   })
+      window.location = './index.html';
+   });
    introBtn.addEventListener('click', function (){
       console.log('click');
       window.location = './introduce.html';
